5cd8ccc133158764c8bea37cb08c1663021c9c51
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2026-02-09 15:04:19 +0000

    Updated doctypes

---
+++ papa_management/papa_management/doctype/library_member/library_member.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Papa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("LIbrary Member", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ papa_management/papa_management/doctype/loan/loan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Papa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Loan", {
+// 	refresh(frm) {
+
+// 	},
+// });
