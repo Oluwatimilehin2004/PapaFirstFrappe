2eb5b4835ce7c218f004c424a2abf4e8979f20fc
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2026-02-12 15:13:53 +0000

    first personal task

---
+++ papa_management/papa_management/doctype/membership_plan/membership_plan.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Papa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MembershipPlan(Document):
+	pass
